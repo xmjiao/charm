ecc6ee4a11c32e56791c667c6230fa9274bd2b93
Nitin Bhat <nbhat4@illinois.edu>
2020-09-01 18:05:15 -0500

    ZC Post API: Delayed rget functionality

--- src/ck-core/ckarray.h
+++ src/ck-core/ckarray.h
@@ -610,6 +610,15 @@ public:
     }
   }
 
+  inline size_t getNumLocalElems() {
+    return localElemVec.size();
+  }
+
+  inline unsigned int getEltLocalIndex(const CmiUInt8 id) {
+    const auto itr = localElems.find(id);
+    return ( itr == localElems.end() ? -1 : itr->second);
+  } 
+
   virtual CkMigratable* getEltFromArrMgr(const CmiUInt8 id) {
     const auto itr = localElems.find(id);
     return ( itr == localElems.end() ? NULL : localElemVec[itr->second] );
@@ -744,6 +753,8 @@ public:
   }
   void flushStates();
   void forwardZCMsgToOtherElems(envelope *env);
+  void forwardZCMsgToSpecificElem(envelope *env, CkMigratable *elem);
+  void forwardZCMsgToZerothElem(envelope *env);
 
 
         static bool isIrreducible() { return true; }
@@ -790,6 +801,7 @@ public:
   int incrementBcastNo();
 
   bool deliver(CkArrayMessage *bcast, ArrayElement *el, bool doFree);
+  bool deliverAlreadyDelivered(CkArrayMessage *bcast, ArrayElement *el, bool doFree);
 #if CMK_CHARM4PY
   void deliver(CkArrayMessage *bcast, std::vector<CkMigratable*> &elements, int arrayId, bool doFree);
 #endif
